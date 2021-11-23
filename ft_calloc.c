ff1d86582875df872dc2f9e5ccd6153669f166f9
Alexandre Lefranc <alefranc.pro@mailo.com>
2021-11-23 11:55:22 +0100

    huge rework to fit actual subject

---
+++ ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alefranc <alefranc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 17:22:41 by alefranc          #+#    #+#             */
+/*   Updated: 2021/11/22 17:43:38 by alefranc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	*calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	if (nmemb * size > 2147483647)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
+}
